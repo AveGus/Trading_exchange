8faf4b59a65f8e9484c6c76fa70a1c647b79ac6a
AveGus(Eldar) <garipov.eldar200@gmail.com>
2025-03-21 20:23:11 +0500

    *Веб-сервер сменён с uvicorn на granian *Проинициализирован Piccolo в качестве ORM(piccolo_conf, piccolo_app) *Добавлена простая модель User(для теста) *Для удобства был добавлен pydantic_settings(settings.py) *Изменена архитектура

---
+++ src/models/users.py
@@ -0,0 +1,10 @@
+from piccolo.table import Table
+from piccolo import columns
+import uuid
+
+
+class User(Table, tablename="user"):
+    id = columns.UUID(primary_key=True, default=uuid.uuid4)
+    email = columns.Varchar(unique=True)
+    name = columns.Varchar()
+    password = columns.Secret(length=255)  # type: ignore
